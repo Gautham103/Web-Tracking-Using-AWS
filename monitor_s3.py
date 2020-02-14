44220f69c0f7282bfe767611555461805f90cb4c
Gautham <gauthamsrinivas103@gmail.com>
2020-02-13 23:54:32 -0600

    Updated EC2 monitor

--- monitor_s3.py
+++ monitor_s3.py
@@ -5,7 +5,7 @@ import os
 
 s3 = boto3.resource('s3')
 storage = boto3.client('s3')
-BUCKET_NAME = 'python-example-bucket'
+BUCKET_NAME = 'python-example-bucket1'
 
 while True:
     bucket = s3.Bucket(BUCKET_NAME)
@@ -20,11 +20,17 @@ while True:
                 bytes = f.read() # read entire file as bytes
                 readable_hash = hashlib.sha256(bytes).hexdigest();
                 print(readable_hash)
-                hash_key = hash_key + "/" + readable_hash
+                hash_key = hash_key + "/hash"
                 print(hash_key)
 
-            storage.upload_file ("/tmp/index.html", BUCKET_NAME, hash_key)
+            with open ("/tmp/hash", "w") as f:
+                f.write(readable_hash)
+
+            storage.upload_file ("/tmp/hash", BUCKET_NAME, hash_key)
             storage.delete_object (Bucket=BUCKET_NAME, Key = obj.key)
             os.remove ("/tmp/index.html")
+            os.remove ("/tmp/hash")
         else:
             print ("Doing Nothing")
+
+
